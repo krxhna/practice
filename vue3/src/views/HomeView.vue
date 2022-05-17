6b168ebb7e4163222d35a4035834bc755c580a2f
krxhna <m.krishnakanth1999@gmail.com>
2022-05-17 15:54:37 +0800

    router added yk

--- vue3/src/views/HomeView.vue
+++ vue3/src/views/HomeView.vue
@@ -1,9 +1,25 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+import { ref } from "vue";
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+
+
+let name = ref(1);
+
+function alert() {
+  name.value = name.value * 2;
+}
+
+function gotonmeow() {
+   router.push("/meow");
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="text-5xl h-screen flex-col items-center flex justify-center">
+    <div class="d">{{ name }}</div>
+    <button class="p-2 bg-green-400" @click="gotonmeow">click</button>
+  </div>
 </template>
