405b825affaace3e4b63235637438f5bceee1f74
krxhna <m.krishnakanth1999@gmail.com>
2022-05-22 23:57:35 +0800

    ionic

---
+++ mobile/myApp/src/views/Tab1Page.vue
@@ -0,0 +1,35 @@
+<template>
+  <ion-page                                                                                                                                                                                                                    >
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 1</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-button color="primary">Primary</ion-button>
+    <ion-content :fullscreen="true" color="light">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 1</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <div style="color:red;">
+      
+      <div>search </div>
+      
+      
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from "@ionic/vue";
+// import ExploreContainer from "@/components/ExploreContainer.vue";
+
+export default defineComponent({
+  name: "Tab1Page",
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+});
+</script>
